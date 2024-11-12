63f7cc8978b47852c279488fb187283d1e226ffe
Giuseppe Futia <giuseppe.futia@graphaware.com>
2024-11-12 10:00:21 +0100

    Remove comments

--- md04/rag/agent_rag.py
+++ md04/rag/agent_rag.py
@@ -17,40 +17,21 @@ class Agent:
         self.vector_index = store.init_vector_store()
         self.model = "gpt-4o-mini"
     
-    def run(self, question: str):
-        """
-        response = self.vector_index.similarity_search_with_score(question, k=3)
-        print(f"\n### Retrieved documents: ")
-        for r in response:
-            print(f"------\nScore: {r[1]}")
-            print(r[0].page_content)
-        """
-      
+    def run(self, question: str):      
         retriever = self.vector_index.as_retriever(k=3, score_threshold=0.7)
         docs = retriever.invoke(question)
         print(f"### Retrieved documents")
         for r in docs:
             print(r.page_content)
         
-        
         vector_qa = RetrievalQA.from_chain_type(
             llm=ChatOpenAI(model_name=self.model, temperature=0.2),
             chain_type="stuff",
-            retriever=self.vector_index.as_retriever(k=3, score_threshold=0.7)
+            retriever=retriever
         )
         response = vector_qa.invoke(question)
         
-        
-        
         print(f"### Agent's response (RAG): {response['result']}")
-        """
-         response = self.vector_index.similarity_search_with_score(question, k=2)
-        print(f"\n### Agent's response: ")
-        for r in response:
-            print(f"------\nScore: {r[1]}")
-            print(r[0].page_content)
-        """
-       
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Run the LangChain Agent with a specified question.")
